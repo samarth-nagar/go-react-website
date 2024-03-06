f524ed8b536f5597ff2b616523c3d63357d587bd
samarth-nagar <samarthxma2009@gmail.com>
2024-03-07 02:59:25 +0530

    added tailwind

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -4,7 +4,9 @@ import FetchData from "./fetchdata.jsx";
 function App() {
   return (
     <div>
-      <h1>My App</h1>
+      <h1 className="text-3xl font-bold underline text-red-500 to-red-300 m-10">
+        Hello world!
+      </h1>
       <FetchData />
     </div>
   );
